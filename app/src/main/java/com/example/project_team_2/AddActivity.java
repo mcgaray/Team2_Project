81b31cc4077b13b590119a1601ea079a1f5150f4
mcgaray <qmbgaray@tip.edu.ph>
2022-12-16 13:07:12 +0800

    Add License

--- app/src/main/java/com/example/project_team_2/AddActivity.java
+++ app/src/main/java/com/example/project_team_2/AddActivity.java
@@ -1,4 +1,20 @@
 package com.example.project_team_2;
 
-public class AddActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_license);
+    }
+    public void back(View view) {
+        Intent intent = new Intent(AddActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
 }
